071529453763a148e19cbf4b676aed977e29b0b3
Abdul Rehman <anis.ghazi@startrum.com>
2021-01-28 15:05:45 +0500

    completed category object

--- model/category.ts
+++ model/category.ts
@@ -1,7 +1,11 @@
 import { Document, Schema } from 'mongoose';
+import * as Joi from 'joi';
+import { ObjectSchema } from 'joi';
+import { validationOptions } from '../util/utils';
 
 export interface ICategory extends Document {
   name: string;
+  description: string;
 }
 
 export const categorySchema = new Schema<ICategory>({
@@ -10,5 +14,20 @@ export const categorySchema = new Schema<ICategory>({
     required: true,
     minlength: 5,
     maxlength: 255
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024
   }
 });
+
+export const validateCategory = (category: ICategory) => {
+  const schema: ObjectSchema<ICategory> = Joi.object<ICategory>({
+    name: Joi.string().min(5).max(255).required(),
+    description: Joi.string().min(5).max(1024).required()
+  });
+
+  return schema.validate(category, validationOptions);
+};
