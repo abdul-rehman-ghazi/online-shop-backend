14d9e3d075ab30ed5cb54112bf06ec7a1f9cbf08
Abdul Rehman <anis.ghazi@startrum.com>
2021-01-28 11:35:22 +0500

    removed unused code

--- startup/routes.ts
+++ startup/routes.ts
@@ -1,21 +1,13 @@
 import express, { Express } from 'express';
-import genres from '../routes/genres';
-import customers from '../routes/customers';
-import movies from '../routes/movies';
 import users from '../routes/users';
 import auth from '../routes/auth';
-import rentals from '../routes/rentals';
 import { error } from '../middleware/error';
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
 
 export default (app: Express) => {
   app.use(express.json());
-  app.use(bodyParser.urlencoded({extended: true}))
-  app.use('/api/genres', genres);
-  app.use('/api/customers', customers);
-  app.use('/api/movies', movies);
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.use('/api/users', users);
   app.use('/api/auth', auth);
-  app.use('/api/rentals', rentals);
   app.use(error);
 };
